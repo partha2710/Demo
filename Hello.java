52eb839556746867070673e75d334a60e85535ef
parthjb <parthbhosale2710@gmail.com>
2025-02-09 20:16:18 +0530

    Update Hello.java
    commiting the Hello.java file from GitHub desktop to main

--- Hello.java
+++ Hello.java
@@ -1,11 +1,12 @@
 class Hello{
   public static void main(String[] args) {
     System.out.print("Hello");
-    
+<<<<<<< HEAD
     System.out.println("Hello from 2nd user");
-
+=======
     System.out.print("Hello from 1st user");
+>>>>>>> 460c5f8e66879ceb629924f460a7af2c8e4b2054
 
-    System.out.print("Hello from GitHub Desktop");
+    System.out.print("Hello from the Desktop");
 }
 }
